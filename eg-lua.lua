35f5fe78c502028710ac1174ac8bf3554231af1d
David Rio Deiros <driodeiros@gmail.com>
2011-03-30 11:30:37 -0500

    * Adding RC to probe table. Modifying printing results accordingly.

--- eg-lua.lua
+++ eg-lua.lua
@@ -24,12 +24,12 @@ end
 
 -- Reverse complement sequence space
 local function ss_reverse_comp(sequence)
-  local t  = { A="T", C="G", G="C", T="A" };
+  local t  = { A="T", C="G", G="C", T="A", N="N"};
   local rs = string.reverse(sequence);
   local rc = "";
 
   for i = 1, #sequence do
-    c = rs:sub(i,i);
+    c  = rs:sub(i,i);
     rc = rc .. t[c];
   end
   return rc;
@@ -38,10 +38,12 @@ end
 -- Load probes
 -- 1	100006955	rs4908018	TTTGTCTAAAACAAC	CTTTCACTAGGCTCA	C	A
 local function load_probes(args)
-  local tmp = {nil, nil, nil, nil, nil, nil, nil}; -- fields in line
-  local h = {};
-  local p = 0; -- Number of probes
-  local probe = nil;
+  local tmp             = {nil, nil, nil, nil, nil, nil, nil}; -- fields in line
+  local h               = {};
+  local p               = 0; -- Number of probes
+  local probe           = nil;
+  local rc_probe        = nil;
+  local do_reverse_comp = ss_reverse_comp;
 
   for line in io.lines(args.probes_fn) do
     if p % cfg.print_lines == 0 then io.stderr:write("\rReading probes: ", p) end
@@ -50,11 +52,18 @@ local function load_probes(args)
       tmp[i] = field;
       i = i + 1;
     end
-    probe          = tmp[4] .. "N" .. tmp[5];
-    h[probe]       = {}
-    h[probe].chrm  = tmp[1]; h[probe].pos  = tmp[2]
-    h[probe].id    = tmp[3]; h[probe].ref  = tmp[6];
-    h[probe].var   = tmp[7]; h[probe].hits = nil;
+    probe            = tmp[4] .. "N" .. tmp[5];
+    rc_probe         = do_reverse_comp(probe);
+    h[probe]         = {}
+    h[rc_probe]      = {} 
+    h[probe].chrm    = tmp[1]; h[probe].pos  = tmp[2]
+    h[probe].id      = tmp[3]; h[probe].ref  = tmp[6];
+    h[probe].var     = tmp[7]; 
+    h[probe].hits    = nil; 
+    h[probe].neg     = false;
+    h[rc_probe].hits = nil;  -- Don't add probe information when rc (save mem)
+    h[rc_probe].neg  = true; -- probe comes from the negative strand
+      
     p = p + 1;
   end
   io.stderr:write("\rReading probes: ", p, "\n");
@@ -112,11 +121,50 @@ end
 
 -- Dumps the allele counting per each probe
 local function show_results(probes)
-  for k,v in pairs(probes) do
-    if v.hits then
-      local h = v.hits
-      io.write(string.format("%s,%s,%s,%s,%s,", v.chrm, v.pos, v.id, v.ref, v.var));
-      io.write(string.format("%s,%s,%s,%s,%s\n", h.A, h.C, h.G, h.T, h.N));
+  local do_reverse_comp = ss_reverse_comp;
+
+  for k,v in pairs(probes) do -- Iterate over the probes
+    if not v.processed then
+      if v.hits then -- If we have hits, report them
+        local info     = nil;
+        local h_pos    = nil; -- hits positive strand
+        local h_neg    = nil; -- hits negative strand
+        local rc_probe = do_reverse_comp(k);
+        local output   = "";
+
+        if v.neg then -- If probe comes from - strand
+          info  = probes[rc_probe];
+          h_neg = v.hits;
+          h_pos = probes[rc_probe].hits or {0,0,0,0,0};
+        else
+          info  = v
+          h_pos = v.hits;
+          h_neg = probes[rc_probe].hits or {0,0,0,0,0};
+        end 
+      
+        -- Make sure we don't process RC probe again
+        probes[rc_probe].processed = true;
+
+        -- probe info
+        output =
+        string.format("%s,%s,%s,%s,%s,",
+          info.chrm, info.pos, info.id, info.ref, info.var) ..
+        -- hits positive strand
+        string.format("%s,%s,%s,%s,%s,",
+          h_pos.A, h_pos.C, h_pos.G, h_pos.T, h_pos.N) ..
+        -- hits negative strand
+        string.format("%s,%s,%s,%s,%s,",
+          h_neg.A, h_neg.C, h_neg.G, h_neg.T, h_neg.N) ..
+        -- hits positive + negative strand
+        string.format("%s,%s,%s,%s,%s\n",
+          h_pos.A + h_neg.A, 
+          h_pos.C + h_neg.C,
+          h_pos.G + h_neg.G, 
+          h_pos.T + h_neg.T, 
+          h_pos.N + h_neg.N);
+
+        io.stdout:write(output); 
+      end
     end
   end  
 end
