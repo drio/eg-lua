5cfc6f76fc7db49f1a5c7da048f29c24526c3ed5
David Rio Deiros <driodeiros@gmail.com>
2011-04-04 09:29:48 -0500

    * nc

---
+++ scripts/run.old.eg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+set -e
+
+old_eg="/Users/drio/Dropbox/git_repo/v1.egenotype/core/eg-counter"
+probes="/Users/drio/Dropbox/git_repo/eg-lua/input/affy.txt"
+reads="/Users/drio/Dropbox/git_repo/rGenotype/data/soldata.txt"
+list="/tmp/tmp.eg.txt"
+
+echo "$reads" > $list
+time $old_eg $probes $list output.old 0.5
+cat output.old | sort -t, -k2,2n > o
+mv o output.old
