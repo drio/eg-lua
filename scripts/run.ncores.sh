2f25ca872e516ee786c6f319adc13e6c3654f8f2
David Rio Deiros <driodeiros@gmail.com>
2011-04-05 14:48:25 -0500

    * Forgot some minor changes before

--- scripts/run.ncores.sh
+++ scripts/run.ncores.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
 set -e
+source "`dirname ${BASH_SOURCE[0]}`/common.sh"
 
-split -l 461229 input/affy.txt 
-$jit $eg_lua ./xaa ./input/soldata.txt > o.xaa &
-$jit $eg_lua ./xab ./input/soldata.txt > o.xab &
+split -l 461229 $probes
+$jit $eg_lua ./xaa $reads > o.xaa &
+$jit $eg_lua ./xab $reads > o.xab &
 wait
 cat o.xaa o.xab | sort -t, -k2,2n > output.split
 rm -f x* o.x*
--- scripts/run.old.eg.sh
+++ scripts/run.old.eg.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 set -e
+source "`dirname ${BASH_SOURCE[0]}`/common.sh"
 
 echo "$reads" > $list
 time $old_eg $probes $list output.old 1
